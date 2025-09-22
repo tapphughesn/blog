f989d3615b16b42ff934b4b3fae0aee0c0824158
Nick Tapp-Hughes <nicholas.tapphughes@gmail.com>
2025-09-22 14:46:58 -0400

    Download link for CV

--- app/src/cv.tsx
+++ app/src/cv.tsx
@@ -4,6 +4,9 @@ function Cv() {
 
     return (
         <div className="CvPage">
+            <div className="CvDownload">
+                You can view the embedded PDF below or <a href="/NickCV_2page_Fall2025.pdf">download it</a> (better on mobile).
+            </div>
             <iframe
                 src="/NickCV_2page_Fall2025.pdf"
                 width="100%"
