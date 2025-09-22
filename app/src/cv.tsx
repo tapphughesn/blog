bbb83c64e1643f895252959623cc43914fb51259
Nick Tapp-Hughes <nicholas.tapphughes@gmail.com>
2025-09-22 14:15:10 -0400

    Add CV page

--- app/src/cv.tsx
+++ app/src/cv.tsx
@@ -3,8 +3,12 @@ import "./App.css"
 function Cv() {
 
     return (
-        <div >
-            CV
+        <div className="CvPage">
+            <iframe
+                src="/NickCV_2page_Fall2025.pdf"
+                width="100%"
+                height="100%"
+            />
         </div>
     );
 }
