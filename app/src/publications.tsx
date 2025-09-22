e1916d7a8470b2ddcda1afe56fa252c636b80bf5
Nick Tapp-Hughes <nicholas.tapphughes@gmail.com>
2025-09-22 15:28:14 -0400

    Add publications section

--- app/src/publications.tsx
+++ app/src/publications.tsx
@@ -3,9 +3,27 @@ import "./App.css"
 function Publications() {
 
     return (
-        <div >
-            Publications
-        </div>
+        <div className="publications-content">
+            <h2 className="publications-header">
+                Publications
+            </h2>
+            <ol className="publications-list">
+                <li>
+                    Stephen M. Pizer, Zhiyuan Liu, Junjie Zhao, Nicholas Tapp-Hughes, James Damon, Miaomiao Zhang, JS Marron, Mohsen Taheri, Jared Vicory,
+                    <br />
+                    "Interior Object Geometry via Fitted Frames,"
+                    <br />
+                    Available: Springer, <a href="https://rdcu.be/eFX0G">https://rdcu.be/eFX0G.</a>
+                </li>
+                <li>
+                    Nicholas Tapp Hughes,
+                    <br />
+                    "A 3d U-net for Segmentation of Subcortical Structures In MR Images of 12 and 24 Month-old Infants,"
+                    <br />
+                    Available: Carolina Digital Repository, <a href="https://cdr.lib.unc.edu/concern/honors_theses/4f16c8286">https://cdr.lib.unc.edu/concern/honors\_theses/4f16c8286. </a>
+                </li>
+            </ol>
+        </div >
     );
 }
 export default Publications;
