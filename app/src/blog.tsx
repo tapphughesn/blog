5c6e39f691c8f04c552124ad47be88485f4df91b
tapphughesn <nicholas.tapphughes@gmail.com>
2026-01-24 16:01:15 -0500

    formatting changes + small content change for Blog site

--- app/src/blog.tsx
+++ app/src/blog.tsx
@@ -3,26 +3,23 @@ import { Link } from 'react-router-dom';
 
 function Blog() {
 
-    return (
+  return (
 
-        <div className="blog-content">
-            <div className="blog-top-section">
-                <h2 >
-                    Blog
-                </h2>
-                <p>
-                    Welcome to my blog! New posts are in the works, and a subscribe button is coming. The posts will be focused on software development, math, and tangential topics.
-                </p>
-            </div>
-            <div className="blog-list">
-                <Link className="post-link" to="/blog/why_write_blog_posts">
-                    <div className="blog-listing">
-                        <h3> Why Write Blog Posts?</h3>
-                        <span>October 12<sup>th</sup> 2025 &middot; 8 minute read</span>
-                    </div>
-                </Link>
-            </div>
-        </div >
-    );
+    <div className="blog-content">
+      <div className="blog-top-section">
+        <p>
+          Welcome to my blog!
+        </p>
+      </div>
+      <div className="blog-list">
+        <Link className="post-link" to="/blog/why_write_blog_posts">
+          <div className="blog-listing">
+            <h3> Why Write Blog Posts?</h3>
+            <span>October 12<sup>th</sup> 2025 &middot; 8 minute read</span>
+          </div>
+        </Link>
+      </div>
+    </div >
+  );
 }
 export default Blog;
