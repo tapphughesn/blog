fa937a7ddee6524968ebf0596334ec33caf4b078
tapphughesn <nicholas.tapphughes@gmail.com>
2026-01-29 12:33:05 -0500

    Created subscribe UI

--- app/src/blog.tsx
+++ app/src/blog.tsx
@@ -1,6 +1,28 @@
 import "./App.css"
 import { Link } from 'react-router-dom';
 
+export function SubscribeComponent() {
+  const onSubscribe = () => {
+
+  };
+
+  return (
+    <div className="subscribe-container">
+      <input
+        type="email"
+        placeholder="Enter email..."
+        className="subscribe-input"
+      />
+      <button
+        onClick={onSubscribe}
+        className="subscribe-button"
+      >
+        Subscribe
+      </button>
+    </div>
+  );
+}
+
 function Blog() {
 
   return (
@@ -8,8 +30,9 @@ function Blog() {
     <div className="blog-content">
       <div className="blog-top-section">
         <p>
-          Welcome to my blog!
+          Welcome to my blog, where I write about interesting technical topics.
         </p>
+        <SubscribeComponent />
       </div>
       <div className="blog-list">
         <Link className="post-link" to="/blog/why_write_blog_posts">
