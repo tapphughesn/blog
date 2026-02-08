9dc13eaeed42ba47175a13c47714d8ce930ab21b
tapphughesn <nicholas.tapphughes@gmail.com>
2026-02-08 11:34:11 -0500

    Don't use seasonal names for resumes

--- app/src/cv.tsx
+++ app/src/cv.tsx
@@ -5,11 +5,11 @@ function Cv() {
   return (
     <div className="CvPage">
       <div className="CvDownload">
-        You can view my CV as an embedded PDF below or <a href="/NickCV_1page_Spr2026.pdf">download it</a> (better on mobile).
+        You can view my CV as an embedded PDF below or <a href="/NickCV_1page.pdf">download it</a> (better on mobile).
       </div>
       <iframe
         className="CvEmbed"
-        src="/NickCV_1page_Spr2026.pdf"
+        src="/NickCV_1page.pdf"
         width="100%"
         height="100%"
       />
