28a0b14520894f52bf5aaf11dfd1b0c6dd63254e
Nick Tapp-Hughes <nicholas.tapphughes@gmail.com>
2026-03-18 10:49:44 -0400

    tweaks to example blog post

--- app/src/blog-posts/an_example_blog_post/meta.ts
+++ app/src/blog-posts/an_example_blog_post/meta.ts
@@ -1,6 +1,6 @@
 export const metadata = {
   title: 'An Example Blog Post',
-  date: 'March 16th, 2026',
-  isoDate: '2026-03-16',
+  date: 'March 18th, 2026',
+  isoDate: '2026-03-18',
   readingTimeMinutes: 1,
 } as const;
