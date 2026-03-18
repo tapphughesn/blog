c97e1840cfff039ba40f56e87db743a1e91609fc
Nick Tapp-Hughes <nicholas.tapphughes@gmail.com>
2026-03-18 14:28:33 -0400

    revisions to buffon

--- app/src/blog-posts/the_delicious_buffons_needle_problem/meta.ts
+++ app/src/blog-posts/the_delicious_buffons_needle_problem/meta.ts
@@ -2,5 +2,5 @@ export const metadata = {
   title: 'The Delicious Buffon’s Needle Problem',
   date: 'March 18th, 2026',
   isoDate: '2026-03-18',
-  readingTimeMinutes: 11,
+  readingTimeMinutes: 20,
 } as const;
